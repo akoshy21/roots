c80f9ccc3daa7bcd330472e19d1ededc049543f9
Annamaria Koshy <annamariakoshy@gmail.com>
2023-02-03 20:55:31 +0000

    roots are drawn at slower speed, have basic split logic down

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+public class Constants
+{
+    public static Vector3 LEFT_DOWN = new Vector3(-1, -1, 0);
+    public static Vector3 RIGHT_DOWN = new Vector3(1, -1, 0);
+}
--- Assets/Scripts/RootController.cs
+++ Assets/Scripts/RootController.cs
@@ -1,11 +1,88 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class RootController: MonoBehaviour
+public class RootController : MonoBehaviour
 {
-    private Coroutine drawing;
+    
+    public float speed = 2f;
+    public float clickDelta = 0.35f;
+
+    public float splitDist = 5f;
+    
     public GameObject rootPrefab;
     public GameObject rootParent;
+
+    private Coroutine drawing;
+
+    private Vector3 screenPoint, original;
+    private Vector3 offset;
+
+    private float timeCount = 0.0f;
+    private float clickTime;
+
+    private bool dragging;
+    private bool click = false;
+    
+
+    void Update()
+    {
+        if (click && Time.time > (clickTime + clickDelta)) {
+            click = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        // Start drawing the root 
+        original = transform.position;
+        StartRoot();
+        offset = gameObject.transform.position -
+                 Camera.main.ScreenToWorldPoint(
+                     new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+        if (click && Time.time <= (clickTime + clickDelta))
+        {
+            // If double click, split the root...
+
+            Vector3 newPosLeft = transform.position + (Constants.LEFT_DOWN * splitDist);
+            Vector3 newPosRight = transform.position + (Constants.RIGHT_DOWN * splitDist);
+
+            GameObject left = Instantiate(gameObject);
+            left.transform.position = newPosLeft;
+
+            transform.position = newPosRight;
+
+            click = false;
+        }
+        else
+        {
+            click = true;
+            clickTime = Time.time;
+        }
+    }
+
+    void OnMouseDrag()
+    {
+        // Object is being dragged.
+        timeCount += Time.deltaTime;
+        if (timeCount > 0.25f)
+        {
+            Debug.Log("Dragging:" + Input.mousePosition);
+            timeCount = 0.0f;
+            dragging = true;
+        }
+
+        if (dragging)
+        {
+            transform.position = original;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        FinishRoot();
+    }
 
     void StartRoot()
     {
@@ -17,19 +94,26 @@ public class RootController: MonoBehaviour
         drawing = StartCoroutine(DrawRoot());
     }
 
-    void FinishLine()
+    void FinishRoot()
     {
-        StopCoroutine(drwaing);
+        StopCoroutine(drawing);
     }
 
-    IEnumerator DrawLine()
+    IEnumerator DrawRoot()
     {
         GameObject go = Instantiate(rootPrefab, rootParent.transform);
         LineRenderer line = go.GetComponent<LineRenderer>();
-    }
 
-    private void StopCoroutine(Coroutine coroutine)
-    {
-        throw new System.NotImplementedException();
+        line.positionCount = 0;
+        while (true)
+        {
+            Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 updated = Vector3.Normalize(position - original) * (speed * Time.deltaTime);
+            position.z = 0;
+            line.positionCount++;
+            original += updated;
+            line.SetPosition(line.positionCount - 1, original);
+            yield return null;
+        }
     }
 }
