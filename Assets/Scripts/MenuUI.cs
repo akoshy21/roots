014565abedd75f124d977299c558f881c98409cc
Benjamin Ramas <benjaminramas119@gmail.com>
2023-02-05 15:22:34 +0000

    Added menu reloading and quitting

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -5,10 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class MenuUI : MonoBehaviour
 {
+    public static bool started=false;
+    public GameObject menuUI;
+
+    private void Start()
+    {
+        Debug.Log("Scene started");
+        if (started)
+        {
+            menuUI.SetActive(false);
+        }
+    }
     public void startGame()
     {
         Scene scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
+        started = true;
     }
 
     public void quitGame()
