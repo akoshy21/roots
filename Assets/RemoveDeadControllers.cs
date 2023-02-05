f7fb724c564e4b5d5387e6a7369892bfbaba35b7
Annamaria Koshy <annamariakoshy@gmail.com>
2023-02-05 16:57:21 +0000

    updates

--- Assets/RemoveDeadControllers.cs
+++ Assets/RemoveDeadControllers.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO: Escape to leave...
+
 public class RemoveDeadControllers : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other)
